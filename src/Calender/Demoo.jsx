2860269802a18de3e672557bd182be634ff8688a
SUKALOLU GANESH <147176578+SukaloluGanesh@users.noreply.github.com>
2025-04-01 00:12:07 +0530

    Demoo.jsx
    backurl

--- src/Calender/Demoo.jsx
+++ src/Calender/Demoo.jsx
@@ -89,7 +89,7 @@ function ToolbarActionsSearch() {
 
   const fetchEvents = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/");
+      const response = await axios.get("https://react-project-backend-tbs6.onrender.com");
       setEvents(response.data);
     } catch (error) {
       console.error("Error fetching events:", error);
